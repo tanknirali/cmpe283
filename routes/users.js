7b5804d329349b9ef3b17e203a2ba29318db5272
nirali-tank <nirali.tank@sjsu.edu>
2015-02-16 12:09:30 -0800

    commit changes for cmpe283

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/userlist', function(req, res, next) {
+router.get('/student', function(req, res, next) {
 	var db = req.db;
-	db.collection('userlist').find().toArray(function(err,items){
+	db.collection('student').find().toArray(function(err,items){
 		res.json(items);
 	});
 });
